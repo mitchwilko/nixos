6be9165dd8084567c73f8ecab675dd84c6c07ea0
mitchwilko <85665443+mitchwilko@users.noreply.github.com>
2026-07-14 23:19:15 +1000

    added mpswserver

--- flake.nix
+++ flake.nix
@@ -33,6 +33,24 @@
         ]; 
       };
 
+      mpswserver = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        modules = [
+          ./hosts/mpswserver/default.nix
+
+          home-manager.nixosModules.home-manager
+
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.mitchw =
+              import ./home/mitchw;
+          }
+        ]; 
+      };
+
       # laptop = nixpkgs.lib.nixosSystem {
       #   system = "x86_64-linux";
 
