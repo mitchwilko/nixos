bc0bf314442d952fe7153d8a4d9a70758317a27f
Mitchell Wilkinson <mitchell01wilkinson@gmail.com>
2026-07-16 18:09:14 +1000

    there exists an issue

--- home/mitchw/vifm/default.nix
+++ home/mitchw/vifm/default.nix
@@ -7,7 +7,7 @@
     enable = true;
   };
 
-  # xdg.configFile."vifm/vifmrc".source = ./vifmrc;
+  xdg.configFile."vifm/vifmrc".source = ./vifmrc;
 
   # xdg.configFile."vifm/colors/myscheme.vifm".source = ./vifm-colors/myscheme.vifm;
 }
