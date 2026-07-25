1c0b9fc178adf4aa978f7cd8ba32beae0a24b0bc
Mitchell Wilkinson <mitchell01wilkinson@gmail.com>
2026-07-25 11:56:18 +1000

    added latex devenv

--- devenvs/latex/flake.nix
+++ devenvs/latex/flake.nix
@@ -1,44 +1,36 @@
-# Wolfram Jupter Notebook
-
 {
-  description = "Jupyter + Wolfram development environment";
+  description = "LaTeX development shell";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
   };
 
   outputs = { self, nixpkgs }:
-  let
-    system = "x86_64-linux";
-
-    pkgs = import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-    };
-
-    python = pkgs.python3.withPackages (ps: with ps; [
-      jupyterlab
-      ipykernel
-
-      numpy
-      scipy
-      matplotlib
-      pandas
-      sympy
-    ]);
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      packages = [
-        python
-        pkgs.wolfram-engine
-        pkgs.gnumake
-        pkgs.libgcc
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
       ];
-
-      shellHook = ''
-        echo "Entering Jupyter/Wolfram Environment"
-        export PS1="(wolfram) $PS1"
-      '';
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs systems (system:
+          f (import nixpkgs { inherit system; }));
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            gnumake
+            (texlive.combine {
+              inherit (texlive)
+                scheme-medium
+                latexmk
+                biber
+                biblatex;
+            })
+          ];
+        };
+      });
     };
-  };
 }
